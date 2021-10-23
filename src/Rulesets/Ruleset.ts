d9acb493cb492f3befd37d2a1e7f19bfe0b25fd0
Kionell <sashakostyunin1@gmail.com>
2021-10-23 10:43:07 +0300

    Added abstract implementation for applying ruleset & mods

--- src/Rulesets/Ruleset.ts
+++ src/Rulesets/Ruleset.ts
@@ -1,6 +1,7 @@
 import { BeatmapProcessor } from '../Beatmaps/BeatmapProcessor';
 import { BeatmapConverter } from '../Beatmaps/BeatmapConverter';
 import { RulesetBeatmap } from '../Beatmaps/RulesetBeatmap';
+import { ModCombination } from '../Mods/ModCombination';
 import { IBeatmap } from '../Beatmaps/IBeatmap';
 import { IRuleset } from './IRuleset';
 
@@ -18,7 +19,90 @@ export abstract class Ruleset implements IRuleset {
    * @param beatmap The beatmap.
    * @returns A new instance of the beatmap with applied ruleset.
    */
-  abstract applyToBeatmap(beatmap: IBeatmap): RulesetBeatmap;
+  applyToBeatmap(beatmap: IBeatmap): RulesetBeatmap {
+    const originalMods = (beatmap as RulesetBeatmap).mods;
+
+    const bitwise = originalMods ? originalMods.bitwise : 0;
+
+    const mods = this.createModCombination(bitwise);
+
+    return this.applyToBeatmapWithMods(beatmap, mods);
+  }
+
+  /**
+   * Applies ruleset and mods to a beatmap.
+   * @param beatmap The beatmap.
+   * @param mods Mod combination.
+   * @returns A new beatmap with applied mods.
+   */
+  applyToBeatmapWithMods(beatmap: IBeatmap, mods?: ModCombination): RulesetBeatmap {
+    if (!mods) {
+      mods = this.createModCombination(0);
+    }
+
+    // We should apply mods only from the same ruleset.
+    if (beatmap.mode !== mods.mode) {
+      mods = this.createModCombination(mods.bitwise);
+    }
+
+    const converter = this.createBeatmapConverter();
+
+    const cloned = beatmap.clone();
+
+    // Check if the beatmap can be converted
+    if (beatmap.hitObjects.length > 0 && !converter.canConvert(cloned)) {
+      throw new Error('Beatmap can not be converted!');
+    }
+
+    // Apply conversion mods
+    mods.converterMods.forEach((m) => m.applyToConverter(converter));
+
+    const converted = converter.convertBeatmap(cloned);
+
+    // Apply difficulty mods
+    mods.difficultyMods.forEach((m) => {
+      m.applyToDifficulty(converted.difficulty);
+    });
+
+    const processor = this.createBeatmapProcessor();
+
+    processor.preProcess(converted);
+
+    // Compute default values for hitobjects, including creating nested hitobjects in-case they're needed
+    converted.hitObjects.forEach((hitObject) => {
+      hitObject.applyDefaults(converted.controlPoints, converted.difficulty);
+    });
+
+    mods.hitObjectMods.forEach((m) => {
+      m.applyToHitObjects(converted.hitObjects);
+    });
+
+    processor.postProcess(converted);
+
+    mods.beatmapMods.forEach((m) => m.applyToBeatmap(converted));
+
+    converted.mods = mods;
+
+    return converted;
+  }
+
+  /**
+   * Resets a mod combination from a beatmap.
+   * @param beatmap The beatmap.
+   * @returns A new beatmap with no mods.
+   */
+  resetMods(beatmap: IBeatmap): RulesetBeatmap {
+    const mods = this.createModCombination(0);
+
+    return this.applyToBeatmapWithMods(beatmap, mods);
+  }
+
+  /**
+   * Creates a new mod combination by converting legacy mod bitwise.
+   * @param bitwise Mod bitwise.
+   * @returns A new mod combination.
+   */
+  abstract createModCombination(bitwise: number): ModCombination;
 
   /**
    * @returns A new beatmap processor.
