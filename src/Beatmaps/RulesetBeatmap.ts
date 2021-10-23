964f6fc79a9822b2a83ea24e91c9cec813b06410
Kionell <sashakostyunin1@gmail.com>
2021-10-23 10:35:25 +0300

    Moved out the logic for applying & resetting mods from beatmap class

--- src/Beatmaps/RulesetBeatmap.ts
+++ src/Beatmaps/RulesetBeatmap.ts
@@ -14,16 +14,4 @@ export abstract class RulesetBeatmap extends Beatmap {
    * Applied mods of a beatmap.
    */
   abstract mods: ModCombination;
-
-  /**
-   * Creates a new mod combination by bitwise and applies it to the beatmap.
-   * @param bitwise Mod bitwise.
-   * @returns The same beatmap with applied mods.
-   */
-  abstract applyMods(bitwise: number): this;
-
-  /**
-   * Resets a mod combination from the beatmap.
-   */
-  abstract resetMods(): this;
 }
