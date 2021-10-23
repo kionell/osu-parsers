d5b4305c71696234934c5272d1d44c12eafb9c0e
Kionell <sashakostyunin1@gmail.com>
2021-10-23 10:42:18 +0300

    Returned mod applying logic to the ruleset interface

--- src/Rulesets/IRuleset.ts
+++ src/Rulesets/IRuleset.ts
@@ -1,6 +1,7 @@
 import { BeatmapProcessor } from '../Beatmaps/BeatmapProcessor';
 import { BeatmapConverter } from '../Beatmaps/BeatmapConverter';
 import { RulesetBeatmap } from '../Beatmaps/RulesetBeatmap';
+import { ModCombination } from '../Mods/ModCombination';
 import { IBeatmap } from '../Beatmaps/IBeatmap';
 
 /**
@@ -18,6 +19,28 @@ export interface IRuleset {
    * @returns A new instance of the beatmap with applied ruleset.
    */
   applyToBeatmap(beatmap: IBeatmap): RulesetBeatmap;
+
+  /**
+   * Applies ruleset and mods to a beatmap.
+   * @param beatmap The beatmap.
+   * @param mods Mod combination.
+   * @returns A new beatmap with applied mods.
+   */
+  applyToBeatmapWithMods(beatmap: IBeatmap, mods: ModCombination): RulesetBeatmap;
+
+  /**
+   * Resets a mod combination from a beatmap.
+   * @param beatmap The beatmap.
+   * @returns The same beatmap.
+   */
+  resetMods(beatmap: RulesetBeatmap): RulesetBeatmap;
+
+  /**
+   * Creates a new mod combination by converting legacy mod bitwise.
+   * @param bitwise Mod bitwise.
+   * @returns A new mod combination.
+   */
+  createModCombination(bitwise: number): ModCombination;
 
   /**
    * @returns A new beatmap processor.
