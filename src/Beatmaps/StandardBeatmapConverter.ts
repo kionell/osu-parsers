e6e18e0e6d2321be218cc8250469575db8548638
Kionell <sashakostyunin1@gmail.com>
2021-12-10 18:33:59 +0200

    Rewritten the converting logic to match core lib

--- src/Beatmaps/StandardBeatmapConverter.ts
+++ src/Beatmaps/StandardBeatmapConverter.ts
@@ -7,9 +7,11 @@ import { Spinner } from '../Objects/Spinner';
 import {
   BeatmapConverter,
   IBeatmap,
-  IHasPath,
-  IHasDuration,
   IHasPosition,
+  IHitObject,
+  ISlidableObject,
+  ISpinnableObject,
+  Vector2,
 } from 'osu-resources';
 
 export class StandardBeatmapConverter extends BeatmapConverter {
@@ -19,37 +21,85 @@ export class StandardBeatmapConverter extends BeatmapConverter {
     });
   }
 
-  *convertHitObjects(original: IBeatmap): Generator<StandardHitObject> {
-    const hitObjects = original.hitObjects;
-    const controlPoints = original.controlPoints;
+  *convertHitObjects(beatmap: IBeatmap): Generator<StandardHitObject> {
+    const hitObjects = beatmap.hitObjects;
 
     for (const hitObject of hitObjects) {
-      if ((hitObject as unknown as IHasPath).path) {
-        const slider = new Slider(hitObject.clone());
-
-        /**
-         * Prior to v8, speed multipliers don't adjust for how many
-         * ticks are generated over the same distance.
-         * This results in more (or less) ticks being generated
-         * in <v8 maps for the same time duration.
-         */
-        slider.tickRate = 1;
-
-        if (original.fileFormat < 8) {
-          const diffPoint = controlPoints.difficultyPointAt(slider.startTime);
-
-          slider.tickRate = Math.fround(1) / diffPoint.speedMultiplier;
-        }
-
-        yield slider;
-      }
-      else if ((hitObject as unknown as IHasDuration).endTime) {
-        yield new Spinner(hitObject.clone());
-      }
-      else {
-        yield new Circle(hitObject.clone());
-      }
+      yield this._convertHitObject(hitObject, beatmap);
     }
+  }
+
+  private _convertHitObject(hitObject: IHitObject, beatmap: IBeatmap) {
+    const slidable = hitObject as ISlidableObject;
+    const spinnable = hitObject as ISpinnableObject;
+
+    if (slidable.path) {
+      return this._convertSlider(slidable, beatmap);
+    }
+
+    if (spinnable.endTime) {
+      return this._convertSpinner(spinnable);
+    }
+
+    return this._convertCircle(hitObject);
+  }
+
+  private _convertCircle(obj: IHitObject): Circle {
+    const converted = new Circle();
+    const posObj = obj as unknown as IHasPosition;
+
+    converted.startPosition = posObj?.startPosition?.clone() ?? new Vector2(0, 0);
+    converted.startTime = obj.startTime;
+    converted.hitType = obj.hitType;
+    converted.hitSound = obj.hitSound;
+    converted.samples = obj.samples.map((s) => s.clone());
+
+    return converted;
+  }
+
+  private _convertSlider(obj: ISlidableObject, beatmap: IBeatmap): Slider {
+    const converted = new Slider();
+    const posObj = obj as unknown as IHasPosition;
+
+    converted.startPosition = posObj?.startPosition?.clone() ?? new Vector2(0, 0);
+    converted.startTime = obj.startTime;
+    converted.hitType = obj.hitType;
+    converted.hitSound = obj.hitSound;
+    converted.repeats = obj.repeats;
+    converted.samples = obj.samples.map((s) => s.clone());
+    converted.nodeSamples = obj.nodeSamples.map((n) => n.map((s) => s.clone()));
+    converted.path = obj.path.clone();
+    converted.legacyLastTickOffset = obj?.legacyLastTickOffset ?? 0;
+
+    /**
+     * Prior to v8, speed multipliers don't adjust for how many
+     * ticks are generated over the same distance.
+     * This results in more (or less) ticks being generated
+     * in <v8 maps for the same time duration.
+     */
+    converted.tickRate = 1;
+
+    if (beatmap.fileFormat < 8) {
+      const diffPoint = beatmap.controlPoints.difficultyPointAt(obj.startTime);
+
+      converted.tickRate = Math.fround(1 / diffPoint.speedMultiplier);
+    }
+
+    return converted;
+  }
+
+  private _convertSpinner(obj: ISpinnableObject): Spinner {
+    const converted = new Spinner();
+    const posObj = obj as unknown as IHasPosition;
+
+    converted.startPosition = posObj?.startPosition?.clone() ?? new Vector2(256, 192);
+    converted.startTime = obj.startTime;
+    converted.endTime = obj.endTime;
+    converted.hitType = obj.hitType;
+    converted.hitSound = obj.hitSound;
+    converted.samples = obj.samples.map((s) => s.clone());
+
+    return converted;
   }
 
   createBeatmap(): StandardBeatmap {
