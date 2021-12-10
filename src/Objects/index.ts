f6f641e815117f937597d1e09cd3e47119f71573
Kionell <sashakostyunin1@gmail.com>
2021-12-10 17:09:00 +0200

    Updated barrels

--- src/Objects/index.ts
+++ src/Objects/index.ts
@@ -1,9 +1,9 @@
 /* Enums */
 export { HitSound } from './Enums/HitSound';
 export { HitType } from './Enums/HitType';
-export { NestedType } from './Enums/NestedType';
 export { PathType } from './Enums/PathType';
 export { SampleSet } from './Enums/SampleSet';
+export { SliderEventType } from './Enums/SliderEventType';
 
 /* Path */
 export { PathApproximator, CircularArcProperties } from './Path/PathApproximator';
@@ -17,19 +17,20 @@ export { SampleBank } from './Sounds/SampleBank';
 /* Types */
 export { IHasColumn } from './Types/IHasColumn';
 export { IHasCombo } from './Types/IHasCombo';
+export { IHasDistance } from './Types/IHasDistance';
 export { IHasDuration } from './Types/IHasDuration';
 export { IHasLegacyLastTickOffset } from './Types/IHasLegacyLastTickOffset';
+export { IHasNodeSamples } from './Types/IHasNodeSamples';
 export { IHasPath } from './Types/IHasPath';
+export { IHasPathWithRepeats } from './Types/IHasPathWithRepeats';
 export { IHasPosition } from './Types/IHasPosition';
+export { IHasRepeats } from './Types/IHasRepeats';
 export { IHasX } from './Types/IHasX';
 export { IHasY } from './Types/IHasY';
 
-/* Objects */
-export { TickGenerator } from './TickGenerator';
+export { EventGenerator, ISliderEventDescriptor } from './EventGenerator';
 export { HitObject } from './HitObject';
 export { IHitObject } from './IHitObject';
 export { IHoldableObject } from './IHoldableObject';
-export { INestedEvent } from './INestedEvent';
-export { INestedHitObject } from './INestedHitObject';
 export { ISlidableObject } from './ISlidableObject';
 export { ISpinnableObject } from './ISpinnableObject';
--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -2,5 +2,4 @@ export { BinarySearch } from './BinarySearch';
 export { Colour } from './Colour';
 export { FastRandom } from './FastRandom';
 export { Interpolation } from './Interpolation';
-export { ObjectCopying } from './ObjectCopying';
 export { Vector2 } from './Vector2';
