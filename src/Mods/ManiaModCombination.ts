ad443860b12bc358f6f244e8e4a06088e98b7082
Kionell <sashakostyunin1@gmail.com>
2021-12-14 04:25:57 +0200

    Move all constructor logic to the abstract class

--- src/Mods/ManiaModCombination.ts
+++ src/Mods/ManiaModCombination.ts
@@ -1,4 +1,4 @@
-import { ModCombination, IMod, ModBitwise } from 'osu-resources';
+import { ModCombination, IMod } from 'osu-resources';
 
 import { ManiaNoMod } from './ManiaNoMod';
 import { ManiaNoFail } from './ManiaNoFail';
@@ -28,115 +28,38 @@ import { ManiaDualStages } from './ManiaDualStages';
 import { ManiaMirror } from './ManiaMirror';
 
 export class ManiaModCombination extends ModCombination {
-  constructor(bitwise?: number) {
-    super();
-
-    if (typeof bitwise !== 'number') {
-      return;
-    }
-
-    const mods: IMod[] = [];
-
-    let currentFlag = 0;
-
-    while (bitwise > 0) {
-      switch (bitwise & (1 << currentFlag)) {
-        case ModBitwise.NoFail:
-          mods.push(new ManiaNoFail());
-          break;
-        case ModBitwise.Easy:
-          mods.push(new ManiaEasy());
-          break;
-        case ModBitwise.Hidden:
-          mods.push(new ManiaHidden());
-          break;
-        case ModBitwise.HardRock:
-          mods.push(new ManiaHardRock());
-          break;
-        case ModBitwise.SuddenDeath:
-          mods.push(new ManiaSuddenDeath());
-          break;
-        case ModBitwise.DoubleTime:
-          mods.push(new ManiaDoubleTime());
-          break;
-        case ModBitwise.HalfTime:
-          mods.push(new ManiaHalfTime());
-          break;
-        case ModBitwise.Nightcore:
-          mods.push(new ManiaNightcore());
-          break;
-        case ModBitwise.Flashlight:
-          mods.push(new ManiaFlashlight());
-          break;
-        case ModBitwise.Autoplay:
-          mods.push(new ManiaAutoplay());
-          break;
-        case ModBitwise.Perfect:
-          mods.push(new ManiaPerfect());
-          break;
-        case ModBitwise.Key4:
-          mods.push(new ManiaKey4());
-          break;
-        case ModBitwise.Key5:
-          mods.push(new ManiaKey5());
-          break;
-        case ModBitwise.Key6:
-          mods.push(new ManiaKey6());
-          break;
-        case ModBitwise.Key7:
-          mods.push(new ManiaKey7());
-          break;
-        case ModBitwise.Key8:
-          mods.push(new ManiaKey8());
-          break;
-        case ModBitwise.FadeIn:
-          mods.push(new ManiaFadeIn());
-          break;
-        case ModBitwise.Random:
-          mods.push(new ManiaRandom());
-          break;
-        case ModBitwise.Cinema:
-          mods.push(new ManiaCinema());
-          break;
-        case ModBitwise.Key9:
-          mods.push(new ManiaKey9());
-          break;
-        case ModBitwise.KeyCoop:
-          mods.push(new ManiaDualStages());
-          break;
-        case ModBitwise.Key1:
-          mods.push(new ManiaKey1());
-          break;
-        case ModBitwise.Key3:
-          mods.push(new ManiaKey3());
-          break;
-        case ModBitwise.Key2:
-          mods.push(new ManiaKey2());
-          break;
-        case ModBitwise.Mirror:
-          mods.push(new ManiaMirror());
-      }
-
-      // Make the processed bit equal to 0.
-      bitwise &= ~(1 << currentFlag++);
-    }
-
-    for (let i = mods.length - 1; i >= 0; --i) {
-      if (this.bitwise & mods[i].incompatibles) {
-        continue;
-      }
-
-      this.all.push(mods[i]);
-    }
-
-    if (!this.all.length) {
-      this.all.push(new ManiaNoMod());
-    }
-
-    this.all.sort((a, b) => a.bitwise - b.bitwise);
-  }
-
   get mode(): number {
     return 3;
+  }
+
+  protected get _availableMods(): IMod[] {
+    return [
+      new ManiaNoMod(),
+      new ManiaNoFail(),
+      new ManiaEasy(),
+      new ManiaHidden(),
+      new ManiaFadeIn(),
+      new ManiaHardRock(),
+      new ManiaSuddenDeath(),
+      new ManiaDoubleTime(),
+      new ManiaHalfTime(),
+      new ManiaNightcore(),
+      new ManiaFlashlight(),
+      new ManiaAutoplay(),
+      new ManiaPerfect(),
+      new ManiaCinema(),
+      new ManiaKey1(),
+      new ManiaKey2(),
+      new ManiaKey3(),
+      new ManiaKey4(),
+      new ManiaKey5(),
+      new ManiaKey6(),
+      new ManiaKey7(),
+      new ManiaKey8(),
+      new ManiaKey9(),
+      new ManiaRandom(),
+      new ManiaDualStages(),
+      new ManiaMirror(),
+    ];
   }
 }
