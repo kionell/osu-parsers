ed46bcabf5de033caf5c0d2a02b8c55749306297
Kionell <sashakostyunin1@gmail.com>
2021-12-18 23:52:25 +0200

    Export IScoreInfo and IHitStatistics interfaces

--- src/Scoring/index.ts
+++ src/Scoring/index.ts
@@ -5,3 +5,5 @@ export { ScoreRank } from './Enums/ScoreRank';
 export { DifficultyRange } from './DifficultyRange';
 export { HitWindows } from './HitWindows';
 export { ScoreInfo } from './ScoreInfo';
+export { IHitStatistics } from './IHitStatistics';
+export { IScoreInfo } from './IScoreInfo';
