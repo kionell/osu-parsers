cb1e855b9e96d165f35742a722d8a16ab8e73700
Kionell <sashakostyunin1@gmail.com>
2022-01-02 23:24:58 +0200

    Add optional constructor and clonning method to the stage definition

--- src/Beatmaps/StageDefinition.ts
+++ src/Beatmaps/StageDefinition.ts
@@ -7,7 +7,11 @@ export class StageDefinition {
   /**
    * The number of columns which this stage contains.
    */
-  columns = 0;
+  columns;
+
+  constructor(columns?: number) {
+    this.columns = columns ?? 0;
+  }
 
   /**
    * Whether the column index is a special column for this stage.
@@ -31,5 +35,13 @@ export class StageDefinition {
     const distanceToEdge = Math.min(column, this.columns - 1 - column);
 
     return distanceToEdge % 2 ? ColumnType.Even : ColumnType.Odd;
+  }
+
+  /**
+   * Creates a new copy of this stage definition.
+   * @returns The copy of this stage definition.
+   */
+  clone(): StageDefinition {
+    return new StageDefinition(this.columns);
   }
 }
