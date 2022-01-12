0d6eb44c2a7eae26f203ae8be86d9a7ac9b0bb25
Kionell <sashakostyunin1@gmail.com>
2022-01-13 00:01:34 +0200

    Fix warning about external packages in rollup bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from '@rollup/plugin-typescript';
+import externals from 'rollup-plugin-node-externals';
 import dts from 'rollup-plugin-dts';
 
 export default [
@@ -8,6 +9,9 @@ export default [
         "removeComments": false,
         "declaration": true,
       }),
+      externals({
+        deps: true,
+      })
     ],
     input: './src/index.ts',
     output: [
