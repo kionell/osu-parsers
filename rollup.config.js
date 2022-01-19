d579ff9d4f31523f32185df4c739cf044437a005
Kionell <sashakostyunin1@gmail.com>
2022-01-19 18:14:41 +0200

    Add support for CommonJS module system

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
+import externals from 'rollup-plugin-node-externals';
+import commonjs from '@rollup/plugin-commonjs';
 import dts from 'rollup-plugin-dts';
 
 export default [
@@ -8,11 +10,14 @@ export default [
         "removeComments": false,
         "declaration": true,
       }),
+      externals({
+        deps: true,
+      }),
     ],
     input: './src/index.ts',
     output: [
       {
-        file: './lib/index.js',
+        file: './lib/esm/index.js',
         format: 'es',
       },
     ],
@@ -21,8 +26,35 @@ export default [
     plugins: [dts()],
     input: './src/index.ts',
     output: {
-      file: './lib/index.d.ts',
+      file: './lib/esm/index.d.ts',
       format: 'es',
+    },
+  },
+  {
+    plugins: [
+      typescript({
+        "removeComments": false,
+        "declaration": true,
+      }),
+      externals({
+        deps: true,
+      }),
+      commonjs(),
+    ],
+    input: './src/index.ts',
+    output: [
+      {
+        file: './lib/cjs/index.js',
+        format: 'cjs',
+      },
+    ],
+  },
+  {
+    plugins: [dts()],
+    input: './src/index.ts',
+    output: {
+      file: './lib/cjs/index.d.ts',
+      format: 'cjs',
     },
   },
 ]
