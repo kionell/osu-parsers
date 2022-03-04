13930f818dec14377d70546d14754d51002bf522
Kionell <sashakostyunin1@gmail.com>
2022-03-04 17:43:05 +0200

    Update some barrel files

--- src/Scoring/index.ts
+++ src/Scoring/index.ts
@@ -1,9 +1,10 @@
-/* Enums */
-export { HitResult } from './Enums/HitResult';
-export { ScoreRank } from './Enums/ScoreRank';
-
-export { DifficultyRange } from './DifficultyRange';
-export { HitWindows } from './HitWindows';
-export { ScoreInfo } from './ScoreInfo';
-export { IScoreInfo } from './IScoreInfo';
-export { IHitStatistics } from './IHitStatistics';
+export * from './DifficultyRange';
+export * from './Enums/HitResult';
+export * from './Enums/ScoreRank';
+export * from './HitWindows';
+export * from './IHitStatistics';
+export * from './IScore';
+export * from './IScoreInfo';
+export * from './Score';
+export * from './ScoreExtensions';
+export * from './ScoreInfo';
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './Beatmaps';
 export * from './Difficulty';
 export * from './Mods';
 export * from './Objects';
+export * from './Replays';
 export * from './Rulesets';
 export * from './Scoring';
 export * from './Storyboards';
