27c703eb6d6f30bcd730de5d75245d656e48767f
kionell <sashakostyunin1@gmail.com>
2022-05-28 23:54:45 +0300

    Add null check to the difficulty calculate method

--- src/Difficulty/DifficultyCalculator.ts
+++ src/Difficulty/DifficultyCalculator.ts
@@ -33,7 +33,7 @@ export abstract class DifficultyCalculator {
    * @returns A structure describing the difficulty of the beatmap.
    */
   calculate(): DifficultyAttributes {
-    const mods = (this._beatmap as RulesetBeatmap).mods;
+    const mods = (this._beatmap as RulesetBeatmap)?.mods;
 
     return this.calculateWithMods(mods ?? this._ruleset.createModCombination());
   }
