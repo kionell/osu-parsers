25adeccf50eb5dc584cdd95f7aed4304339b33bc
kionell <sashakostyunin1@gmail.com>
2022-05-29 06:36:41 +0300

    Add description to the difficulty attributes properties

--- src/Difficulty/Attributes/ManiaDifficultyAttributes.ts
+++ src/Difficulty/Attributes/ManiaDifficultyAttributes.ts
@@ -1,7 +1,15 @@
 ﻿import { DifficultyAttributes } from 'osu-classes';
 
 export class ManiaDifficultyAttributes extends DifficultyAttributes {
+  /**
+   * The hit window for a GREAT hit inclusive of rate-adjusting mods (DT/HT/etc).
+   * Rate-adjusting mods do not affect the hit window at all in osu-stable.
+   */
   greatHitWindow = 0;
+
+  /**
+   * The score multiplier applied via score-reducing mods.
+   */
   scoreMultiplier = 0;
 
   *toDatabaseAttributes(): Generator<[number, number]> {
