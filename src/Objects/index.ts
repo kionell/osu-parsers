2effe20f2a53046d1f8351645657685f7337ad09
kionell <sashakostyunin1@gmail.com>
2022-07-02 22:33:52 +0300

    Fix barrel files

--- src/Objects/index.ts
+++ src/Objects/index.ts
@@ -1,36 +1,16 @@
-/* Enums */
-export { HitSound } from './Enums/HitSound';
-export { HitType } from './Enums/HitType';
-export { PathType } from './Enums/PathType';
-export { SampleSet } from './Enums/SampleSet';
-export { SliderEventType } from './Enums/SliderEventType';
-
-/* Path */
-export { PathApproximator, CircularArcProperties } from './Path/PathApproximator';
-export { PathPoint } from './Path/PathPoint';
-export { SliderPath } from './Path/SliderPath';
-
-/* Sounds */
-export { HitSample } from './Sounds/HitSample';
-export { SampleBank } from './Sounds/SampleBank';
-
-/* Types */
-export { IHasColumn } from './Types/IHasColumn';
-export { IHasCombo } from './Types/IHasCombo';
-export { IHasDistance } from './Types/IHasDistance';
-export { IHasDuration } from './Types/IHasDuration';
-export { IHasLegacyLastTickOffset } from './Types/IHasLegacyLastTickOffset';
-export { IHasNodeSamples } from './Types/IHasNodeSamples';
-export { IHasPath } from './Types/IHasPath';
-export { IHasPathWithRepeats } from './Types/IHasPathWithRepeats';
-export { IHasPosition } from './Types/IHasPosition';
-export { IHasRepeats } from './Types/IHasRepeats';
-export { IHasX } from './Types/IHasX';
-export { IHasY } from './Types/IHasY';
-
-export { EventGenerator, ISliderEventDescriptor } from './EventGenerator';
-export { HitObject } from './HitObject';
-export { IHitObject } from './IHitObject';
-export { IHoldableObject } from './IHoldableObject';
-export { ISlidableObject } from './ISlidableObject';
-export { ISpinnableObject } from './ISpinnableObject';
+export * from './Enums/HitSound';
+export * from './Enums/HitType';
+export * from './Enums/PathType';
+export * from './Enums/SampleSet';
+export * from './Enums/SliderEventType';
+export * from './EventGenerator';
+export * from './HitObject';
+export * from './IHitObject';
+export * from './IHoldableObject';
+export * from './ISlidableObject';
+export * from './ISpinnableObject';
+export * from './Path/PathApproximator';
+export * from './Path/PathPoint';
+export * from './Path/SliderPath';
+export * from './Sounds';
+export * from './Types';
