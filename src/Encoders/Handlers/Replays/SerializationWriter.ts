4580accac19bd07bea872313d363b0302d076b03
kionell <sashakostyunin1@gmail.com>
2022-07-15 19:21:51 +0300

    Rewrite serial writer to finally work

--- src/Encoders/Handlers/Replays/SerializationWriter.ts
+++ src/Encoders/Handlers/Replays/SerializationWriter.ts
@@ -1,51 +1,55 @@
 export class SerializationWriter {
   /**
-   * Data buffer for writing.
-   */
-  buffer: Buffer;
-
-  /**
    * Number of written bytes.
    */
   private _bytesWritten = 0;
+
+  /**
+   * Temp buffers for writing.
+   */
+  private _buffers: Buffer[] = [];
 
   get bytesWritten(): number {
     return this._bytesWritten;
   }
 
-  constructor(buffer: Buffer) {
-    this.buffer = buffer;
+  finish(): Buffer {
+    return Buffer.concat(this._buffers);
   }
 
   writeByte(value: number): number {
-    this._bytesWritten = this.buffer.writeUint8(value, this._bytesWritten);
+    const buffer = Buffer.alloc(1);
+    const bytesWritten = buffer.writeUint8(value);
 
-    return this._bytesWritten;
+    return this._update(bytesWritten, buffer);
   }
 
   writeBytes(value: Buffer): number {
-    this.buffer = Buffer.concat([this.buffer, value]);
     this._bytesWritten += value.byteLength;
+    this._buffers.push(value);
 
-    return this._bytesWritten;
+    return value.byteLength;
   }
 
   writeShort(value: number): number {
-    this._bytesWritten = this.buffer.writeUInt16LE(value, this._bytesWritten);
+    const buffer = Buffer.alloc(2);
+    const bytesWritten = buffer.writeUInt16LE(value);
 
-    return this._bytesWritten;
+    return this._update(bytesWritten, buffer);
   }
 
   writeInteger(value: number): number {
-    this._bytesWritten = this.buffer.writeInt32LE(value, this._bytesWritten);
+    const buffer = Buffer.alloc(4);
+    const bytesWritten = buffer.writeInt32LE(value);
 
-    return this._bytesWritten;
+    return this._update(bytesWritten, buffer);
   }
 
   writeLong(value: bigint): number {
-    this._bytesWritten = this.buffer.writeBigInt64LE(value, this._bytesWritten);
+    const buffer = Buffer.alloc(8);
+    const bytesWritten = buffer.writeBigInt64LE(value);
 
-    return this._bytesWritten;
+    return this._update(bytesWritten, buffer);
   }
 
   writeDate(date: Date): number {
@@ -53,20 +57,53 @@ export class SerializationWriter {
     const epochTicks = BigInt(62135596800000);
 
     // The number of seconds in ticks.
-    const ticks = BigInt(date.getTime()) * BigInt(1e4);
+    const ticks = BigInt(date.getTime());
 
-    this._bytesWritten = this.writeLong(ticks + epochTicks);
-
-    return this._bytesWritten;
+    return this.writeLong((ticks + epochTicks) * BigInt(1e4));
   }
 
   writeString(value: string): number {
-    for (let i = 0; i < value.length; ++i) {
-      this._bytesWritten = this.writeByte(value.charCodeAt(i));
+    if (value.length === 0) {
+      return this.writeByte(0x00);
     }
 
-    this._bytesWritten = this.writeByte(0);
+    let bytesWritten = this.writeByte(0x0b);
 
-    return this._bytesWritten;
+    bytesWritten += this.writeULEB128(value.length);
+
+    for (let i = 0; i < value.length; ++i) {
+      bytesWritten += this.writeByte(value.charCodeAt(i));
+    }
+
+    return bytesWritten;
+  }
+
+  /**
+   * Write ULEB128.
+   */
+  writeULEB128(value: number): number {
+    let byte = 0;
+    let bytesWritten = 0;
+
+    do {
+      byte = value & 0x7F;
+      value >>= 7;
+
+      if (value !== 0) {
+        byte |= 0x80;
+      }
+
+      bytesWritten += this.writeByte(byte);
+    }
+    while (value !== 0);
+
+    return bytesWritten;
+  }
+
+  private _update(bytesWritten: number, buffer: Buffer): number {
+    this._bytesWritten += bytesWritten;
+    this._buffers.push(buffer);
+
+    return bytesWritten;
   }
 }
