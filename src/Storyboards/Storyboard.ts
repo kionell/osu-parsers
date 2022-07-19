5d69a4057536ef8af67420df0aa9ebc1f81faf68
kionell <sashakostyunin1@gmail.com>
2022-07-19 15:12:14 +0300

    Reorganize stuff in storyboard class and mark old implementation as deprecated

--- src/Storyboards/Storyboard.ts
+++ src/Storyboards/Storyboard.ts
@@ -1,49 +1,100 @@
 import { IStoryboardElement } from './Elements/Types/IStoryboardElement';
 import { LayerType } from './Enums/LayerType';
+import { StoryboardLayer } from './StoryboardLayer';
 
 /**
  * A beatmap storyboard.
  */
 export class Storyboard {
   /**
-   * The background layer of the storyboard. 
+   * The foreground layer of the storyboard.
+   * @deprecated
    */
-  background: IStoryboardElement[] = [];
-
-  /**
-   * The fail layer of the storyboard. 
-   */
-  fail: IStoryboardElement[] = [];
+  foreground: IStoryboardElement[];
 
   /**
    * The pass layer of the storyboard. 
+   * @deprecated
    */
-  pass: IStoryboardElement[] = [];
+  pass: IStoryboardElement[];
 
   /**
-   * The foreground layer of the storyboard. 
+   * The fail layer of the storyboard.
+   * @deprecated
    */
-  foreground: IStoryboardElement[] = [];
+  fail: IStoryboardElement[];
 
   /**
-   * The overlay layer of the storyboard. 
+   * The background layer of the storyboard.
+   * @deprecated 
    */
-  overlay: IStoryboardElement[] = [];
+  background: IStoryboardElement[];
 
   /**
-   * Samples of the storyboard. 
+   * The overlay layer of the storyboard.
+   * @deprecated
    */
-  samples: IStoryboardElement[] = [];
+  overlay: IStoryboardElement[];
 
   /**
-   * Variables of the storyboard. 
+   * Samples of the storyboard.
+   * @deprecated
    */
-  variables: { [key: string]: string } = {};
+  samples: IStoryboardElement[];
 
   /**
-   * Finds a storyboard layer by its type.
+   * Variables of the storyboard.
+   */
+  variables: Record<string, string> = {};
+
+  /**
+   * Storyboard layers.
+   */
+  private _layers: Map<string, StoryboardLayer> = new Map();
+
+  /**
+   * Whether the storyboard can fall back to skin sprites 
+   * in case no matching storyboard sprites are found.
+   */
+  useSkinSprites = false;
+
+  /**
+   * Depth of the currently front-most storyboard layer, excluding the overlay layer.
+   */
+  minimumLayerDepth = 0;
+
+  constructor() {
+    const foregroundLayer = new StoryboardLayer('Foreground', LayerType.Foreground);
+    const passLayer = new StoryboardLayer('Pass', LayerType.Pass);
+    const failLayer = new StoryboardLayer('Fail', LayerType.Fail);
+    const backgroundLayer = new StoryboardLayer('Background', LayerType.Background);
+    const videoLayer = new StoryboardLayer('Video', LayerType.Video, false);
+    const overlayLayer = new StoryboardLayer('Overlay', LayerType.Overlay);
+
+    failLayer.visibleWhenPassing = false;
+    passLayer.visibleWhenFailing = false;
+
+    this._layers.set(foregroundLayer.name, foregroundLayer);
+    this._layers.set(passLayer.name, passLayer);
+    this._layers.set(failLayer.name, failLayer);
+    this._layers.set(backgroundLayer.name, backgroundLayer);
+    this._layers.set(videoLayer.name, videoLayer);
+    this._layers.set(overlayLayer.name, overlayLayer);
+
+    // TODO: Remove this in next versions.
+    this.foreground = foregroundLayer.elements;
+    this.pass = passLayer.elements;
+    this.fail = failLayer.elements;
+    this.background = backgroundLayer.elements;
+    this.overlay = overlayLayer.elements;
+    this.samples = [];
+  }
+
+  /**
+   * Finds a storyboard layer by its type. Otherwise returns background layer.
    * @param type The type of the storyboard layer.
    * @returns The storyboard layer.
+   * @deprecated
    */
   getLayer(type: LayerType): IStoryboardElement[] {
     switch (type) {
@@ -64,5 +115,28 @@ export class Storyboard {
     }
 
     return this.background;
+  }
+
+  /**
+   * Finds a storyboard layer by its type. Returns background layer if type doesn't exist.
+   * @param type The type of the storyboard layer.
+   * @returns The storyboard layer.
+   */
+  getLayerByType(type: LayerType): StoryboardLayer {
+    return this.getLayerByName(LayerType[type] ?? 'Background');
+  }
+
+  /**
+   * Finds a storyboard layer by its name. Otherwise will create a new storyboard layer with this name.
+   * @param name The name of the storyboard layer.
+   * @returns The storyboard layer.
+   */
+  getLayerByName(name: string): StoryboardLayer {
+    const layer = this._layers.get(name)
+      ?? new StoryboardLayer(name, --this.minimumLayerDepth);
+
+    if (!this._layers.has(name)) this._layers.set(name, layer);
+
+    return layer;
   }
 }
