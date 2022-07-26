bfdd76f0253e7756cf82e8c52f31d5c576a8493f
kionell <sashakostyunin1@gmail.com>
2022-07-26 21:10:17 +0300

    Revert default layer type order

--- src/Storyboards/Enums/LayerType.ts
+++ src/Storyboards/Enums/LayerType.ts
@@ -2,11 +2,10 @@
  * Types of storyboard layers.
  */
 export enum LayerType {
-  Overlay = -2147483648, // Min Int32 value.
-  Foreground = 0,
-  Pass,
-  Fail,
   Background,
+  Fail,
+  Pass,
+  Foreground,
+  Overlay,
   Video,
-  Samples,
 }
