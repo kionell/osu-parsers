ef4c70c71c46ba7d02980343ef285749d0d4d331
kionell <sashakostyunin1@gmail.com>
2022-07-29 10:04:19 +0300

    Move beatmap storyboard encoding logic to the storyboard event encoder

--- src/Encoders/Handlers/Beatmaps/BeatmapEventEncoder.ts
+++ src/Encoders/Handlers/Beatmaps/BeatmapEventEncoder.ts
@@ -1,4 +1,5 @@
-import { IBeatmap } from 'osu-classes';
+import { EventType, IBeatmap } from 'osu-classes';
+import { StoryboardEventEncoder } from '../Storyboards/StoryboardEventEncoder';
 
 /**
  * An encoder for beatmap events.
@@ -13,65 +14,26 @@ export abstract class BeatmapEventEncoder {
     const encoded: string[] = [];
 
     const events = beatmap.events;
-    const storyboard = events.storyboard;
 
     encoded.push('[Events]');
-
     encoded.push('//Background and Video events');
 
-    if (events.background) {
-      encoded.push(`0,0,"${events.background}",0,0`);
-    }
-
-    if (events.video) {
-      encoded.push(`Video,${events.videoOffset},"${events.video}"`);
+    if (events.backgroundPath) {
+      encoded.push(`0,0,"${events.backgroundPath}",0,0`);
     }
 
     encoded.push('//Break Periods');
 
     if (events.breaks && events.breaks.length > 0) {
       events.breaks.forEach((b) => {
-        encoded.push(`2,${b.startTime},${b.endTime}`);
+        encoded.push(`${EventType[EventType.Break]},${b.startTime},${b.endTime}`);
       });
     }
 
-    encoded.push('//Storyboard Layer 0 (Background)');
-
-    if (storyboard && storyboard.background.length > 0) {
-      encoded.push(EventsEncoder.encodeStoryboardLayer(storyboard.background));
-    }
-
-    encoded.push('//Storyboard Layer 1 (Fail)');
-
-    if (storyboard && storyboard.fail.length > 0) {
-      encoded.push(EventsEncoder.encodeStoryboardLayer(storyboard.fail));
-    }
-
-    encoded.push('//Storyboard Layer 2 (Pass)');
-
-    if (storyboard && storyboard.pass.length > 0) {
-      encoded.push(EventsEncoder.encodeStoryboardLayer(storyboard.pass));
-    }
-
-    encoded.push('//Storyboard Layer 3 (Foreground)');
-
-    if (storyboard && storyboard.foreground.length > 0) {
-      encoded.push(EventsEncoder.encodeStoryboardLayer(storyboard.foreground));
-    }
-
-    encoded.push('//Storyboard Layer 4 (Overlay)');
-
-    if (storyboard && storyboard.overlay.length > 0) {
-      encoded.push(EventsEncoder.encodeStoryboardLayer(storyboard.overlay));
-    }
-
-    encoded.push('//Storyboard Sound Samples');
-
-    if (storyboard && storyboard.samples.length > 0) {
-      encoded.push(EventsEncoder.encodeStoryboardLayer(storyboard.samples));
+    if (events.storyboard) {
+      encoded.push(StoryboardEventEncoder.encodeStoryboard(events.storyboard));
     }
 
     return encoded.join('\n');
-  }
   }
 }
