d333256d0b3e3c4069ba2e54238e362bc587b0bf
kionell <sashakostyunin1@gmail.com>
2022-08-04 03:02:01 +0300

    Bind Buffer to the window global object in the browser environment

--- src/browser/Utils/Buffer.ts
+++ src/browser/Utils/Buffer.ts
@@ -1,1 +1,11 @@
-import 'buffer/';
+import { Buffer as WebBuffer } from 'buffer/';
+
+declare global {
+  interface Window {
+    Buffer: WebBuffer;
+  }
+}
+
+if (typeof window !== 'undefined') {
+  window.Buffer = WebBuffer as any;
+}
