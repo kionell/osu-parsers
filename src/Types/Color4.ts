9a5f03ab2de272cd4eac58023d1ffaf8bc6affa5
kionell <sashakostyunin1@gmail.com>
2022-08-17 11:40:51 +0300

    Change default color values to be 255,255,255,1

--- src/Types/Color4.ts
+++ src/Types/Color4.ts
@@ -3,49 +3,55 @@
  */
 export class Color4 {
   /**
-   * The red value of the color.
+   * The red value of the color in range from 0 to 255.
+   * @default 255
    */
   red: number;
 
   /**
-   * The green value of the color.
+   * The green value of the color in range from 0 to 255.
+   * @default 255
    */
   green: number;
 
   /**
-   * The blue value of the color.
+   * The blue value of the color in range from 0 to 255.
+   * @default 255
    */
   blue: number;
 
   /**
-   * The alpha value of the color.
+   * The alpha value of the color in range from 0 to 1.
+   * @default 1
    */
   alpha: number;
 
   /**
    * Creates a new color.
-   * @param red Red value of the color.
-   * @param green Green value of the color.
-   * @param blue Blue value of the color.
-   * @param alpha Alpha value of the color.
+   * @param red Red value of the color in range from 0 to 255.
+   * @param green Green value of the color in range from 0 to 255.
+   * @param blue Blue value of the color in range from 0 to 255.
+   * @param alpha Alpha value of the color in range from 0 to 1.
    */
   constructor(red?: number, green?: number, blue?: number, alpha?: number) {
-    this.red = red || 0;
-    this.green = green || 0;
-    this.blue = blue || 0;
-    this.alpha = alpha || 255;
+    this.red = red ?? 255;
+    this.green = green ?? 255;
+    this.blue = blue ?? 255;
+    this.alpha = alpha ?? 1;
   }
 
   /**
    * The HEX representation of the color.
    */
   get hex(): string {
+    const alpha = Math.round(this.alpha * 255);
+
     return (
       '#' +
       this.red.toString(16).padStart(2, '0') +
       this.green.toString(16).padStart(2, '0') +
       this.blue.toString(16).padStart(2, '0') +
-      this.alpha.toString(16).padStart(2, '0')
+      alpha.toString(16).padStart(2, '0')
     );
   }
 
