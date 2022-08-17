a3c2bf5bace6addbf707683f081345fab560aa19
kionell <sashakostyunin1@gmail.com>
2022-08-18 01:27:56 +0300

    Remove deprecated stuff

--- src/Beatmaps/TaikoBeatmap.ts
+++ src/Beatmaps/TaikoBeatmap.ts
@@ -1,7 +1,6 @@
+import { RulesetBeatmap, HitType } from 'osu-classes';
 import { TaikoModCombination } from '../Mods/TaikoModCombination';
 import { TaikoHitObject } from '../Objects/TaikoHitObject';
-
-import { RulesetBeatmap, HitType } from 'osu-classes';
 
 export class TaikoBeatmap extends RulesetBeatmap {
   mods: TaikoModCombination = new TaikoModCombination();
--- src/Difficulty/Attributes/TaikoDifficultyAttributes.ts
+++ src/Difficulty/Attributes/TaikoDifficultyAttributes.ts
@@ -6,16 +6,4 @@ export class TaikoDifficultyAttributes extends DifficultyAttributes {
   colourStrain = 0;
   approachRate = 0;
   greatHitWindow = 0;
-
-  *toDatabaseAttributes(): Generator<[number, number]> {
-    yield [DifficultyAttributes.ATTRIB_ID_MAX_COMBO, this.maxCombo];
-    yield [DifficultyAttributes.ATTRIB_ID_STRAIN, this.starRating];
-    yield [DifficultyAttributes.ATTRIB_ID_GREAT_HIT_WINDOW, this.greatHitWindow];
-  }
-
-  fromDatabaseAttributes(values: {[key: number]: number}): void {
-    this.maxCombo = +values[DifficultyAttributes.ATTRIB_ID_MAX_COMBO];
-    this.starRating = values[DifficultyAttributes.ATTRIB_ID_STRAIN];
-    this.greatHitWindow = values[DifficultyAttributes.ATTRIB_ID_GREAT_HIT_WINDOW];
-  }
 }
