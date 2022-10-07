d532ce05318a317812fccabb98072ac81e02ca9f
Alexander <sashakostyunin1@gmail.com>
2022-10-07 20:07:15 +0300

    Use array of constant size for difficulty hit objects

--- src/Difficulty/StandardDifficultyCalculator.ts
+++ src/Difficulty/StandardDifficultyCalculator.ts
@@ -116,7 +116,7 @@ export class StandardDifficultyCalculator extends DifficultyCalculator<StandardD
   }
 
   protected _createDifficultyHitObjects(beatmap: IBeatmap, clockRate: number): StandardDifficultyHitObject[] {
-    const difficultyObjects: StandardDifficultyHitObject[] = [];
+    const difficultyObjects = new Array(beatmap.hitObjects.length);
 
     /**
      * The first jump is formed by the first two hitobjects of the map.
