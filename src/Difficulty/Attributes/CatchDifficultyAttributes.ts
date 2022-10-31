0a85c2030ca2b7b94f8ba114b76afd6a5fb97038
Alexander <sashakostyunin1@gmail.com>
2022-10-31 15:01:22 +0200

    Add description to the approachRate attribute in difficulty attributes

--- src/Difficulty/Attributes/CatchDifficultyAttributes.ts
+++ src/Difficulty/Attributes/CatchDifficultyAttributes.ts
@@ -1,5 +1,10 @@
 ﻿import { DifficultyAttributes } from 'osu-classes';
 
 export class CatchDifficultyAttributes extends DifficultyAttributes {
+  /**
+   * The perceived approach rate inclusive of rate-adjusting mods (DT/HT/etc).
+   * Rate-adjusting mods don't directly affect the approach rate difficulty value, 
+   * but have a perceived effect as a result of adjusting audio timing.
+   */
   approachRate = 0;
 }
