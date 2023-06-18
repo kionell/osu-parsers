dee64b61c0828ff255126ae266a6afa564028dd6
Alexander <sashakostyunin1@gmail.com>
2023-06-18 22:30:41 +0300

    Fix comment

--- src/Replays/TaikoReplayConverter.ts
+++ src/Replays/TaikoReplayConverter.ts
@@ -2,7 +2,7 @@ import { IReplayFrame, ReplayConverter } from 'osu-classes';
 import { TaikoReplayFrame } from './TaikoReplayFrame';
 
 /**
- * osu!std replay converter.
+ * osu!taiko replay converter.
  */
 export class TaikoReplayConverter extends ReplayConverter {
   protected _createConvertibleReplayFrame(): TaikoReplayFrame {
